523ff5b673e418a2cf8daa995c2377aa2ac099c6
blonk@spotify.com <blonk@spotify.com>
2018-10-24 15:23:02 -0400

    wip

--- main.py
+++ main.py
@@ -9,19 +9,13 @@ from slideshow import slideshow_main
 from test import test_main
 from utils import get_ip_address, blink_onboard_led, reset_onboard_led, get_serial_pixel, read_test_switch
 
-root = logging.getLogger()
-root.setLevel(logging.DEBUG)
-
-ch = logging.StreamHandler(sys.stderr)
-ch.setLevel(logging.DEBUG)
-
-root.addHandler(ch)
-
-logging.error('error')
+print('starting...')
 reset_onboard_led()
 
 if __name__ == '__main__':
     if read_test_switch():
+        print('running test pattern')
         test_main()
     else:
+        print('running slideshow')
         slideshow_main();
