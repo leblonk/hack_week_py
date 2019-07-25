645107cc25e90daff62ddf30e9d765a068b765f4
blonk@spotify.com <blonk@spotify.com>
2019-07-25 16:05:21 -0400

    wip

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #! /bin/sh
 
 mkdir ~/logs
-sudo chmod 755 /home/pi/hack_week_py/run.sh
+cp /home/pi/hack_week_py/run.sh /home/pi/run.sh
+sudo chmod 755 /home/pi/run.sh
 sudo cp /home/pi/hack_week_py/leddisplay.service /lib/systemd/system/leddisplay.service
 sudo chmod 644 /lib/systemd/system/leddisplay.service
 sudo systemctl daemon-reload
