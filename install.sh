17c5042ffd5310ed348f5f0c988b929755efc77a
blonk@spotify.com <blonk@spotify.com>
2019-07-25 16:01:15 -0400

    wip

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+mkdir ~/logs
 sudo chmod 755 /home/pi/hack_week_py/run.sh
 sudo cp /home/pi/hack_week_py/leddisplay.service /lib/systemd/system/leddisplay.service
 sudo chmod 644 /lib/systemd/system/leddisplay.service
--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-
 if [ ! -d /home/pi/hack_week_py ]; then
     cd /home/pi
     sudo git clone https://github.com/leblonk/hack_week_py
@@ -7,4 +6,4 @@ else
     cd /home/pi/hack_week_py
     sudo git pull origin master
 fi
-GOOGLE_APPLICATION_CREDENTIALS=/home/pi/.ssh/bucket.json sudo -E python /home/pi/hack_week_py/main.py 2>&1 > /home/pi/hack_week_py/out.log
+GOOGLE_APPLICATION_CREDENTIALS=/home/pi/.ssh/bucket.json sudo -E python /home/pi/hack_week_py/main.py 2>&1 > /home/pi/logs/out.log
