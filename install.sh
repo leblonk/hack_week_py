6c281ebd484bcce6df815f216c6cbafbaa2ec2c6
blonk@spotify.com <blonk@spotify.com>
2019-07-25 15:20:33 -0400

    wip

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#! /bin/sh
+
 sudo chmod 755 /home/pi/hack_week_py/run.sh
 sudo cp /home/pi/hack_week_py/leddisplay.service /lib/systemd/system/leddisplay.service
 sudo chmod 644 /lib/systemd/system/leddisplay.service
